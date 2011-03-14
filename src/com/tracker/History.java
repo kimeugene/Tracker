c13c076e42bb65edf604644518286595d8e4547d
Eugene <eugene@pictela.com>
2011-03-13 21:16:08 -0400

    added events list

--- src/com/tracker/History.java
+++ src/com/tracker/History.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
 
 public class History extends Activity {
     /** Called when the activity is first created. */
@@ -20,5 +23,22 @@ public class History extends Activity {
                 startActivityForResult(myIntent, 0);
             }
         });
+        
+        TextView view = (TextView) findViewById(R.id.history);
+
+        DbOpenHelper dbOpenHelper = new DbOpenHelper(History.this);
+	    SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+        db.setVersion(1);
+        db.setLockingEnabled(true);
+        
+        Cursor cur = db.query("history", 
+       		null, null, null, null, null, null);
+        cur.moveToFirst();
+        while (cur.isAfterLast() == false) {
+            view.append("\n" + cur.getString(1));
+       	    cur.moveToNext();
+        }
+        cur.close();
+
     }
 }
--- src/com/tracker/Main.java
+++ src/com/tracker/Main.java
@@ -19,23 +19,23 @@ public class Main extends Activity {
         
         Button history_btn = (Button) findViewById(R.id.history_btn);
         history_btn.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View view) {
-		    Intent myIntent = new Intent(view.getContext(), History.class);
-		    startActivityForResult(myIntent, 0);
-		}
+			public void onClick(View view) {
+			    Intent myIntent = new Intent(view.getContext(), History.class);
+			    startActivityForResult(myIntent, 0);
+			}
 	    });
         
         Button save_event_btn = (Button) findViewById(R.id.save_event_btn);
         
         save_event_btn.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View view) {
-		    DbOpenHelper dbOpenHelper = new DbOpenHelper(Main.this);
-		    SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
-		    ContentValues cv = new ContentValues();
-		    cv.put(DbOpenHelper.DATE, new Date().toString());
-		    db.insert(DbOpenHelper.TABLE_NAME, null, cv);
-		    db.close();
-		}
+			public void onClick(View view) {
+			    DbOpenHelper dbOpenHelper = new DbOpenHelper(Main.this);
+			    SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+			    ContentValues cv = new ContentValues();
+			    cv.put(DbOpenHelper.DATE, new Date().toString());
+			    db.insert(DbOpenHelper.TABLE_NAME, null, cv);
+			    db.close();
+			}
 	    });
     }
 }
