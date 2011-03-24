23f6b4f2c92a4cf6b21c47e273c8ff918f5a36f3
unknown <tdenis@.progers.com>
2011-03-24 17:25:17 +0500

    add timer

--- gen/com/tracker/R.java
+++ gen/com/tracker/R.java
@@ -18,6 +18,7 @@ public final class R {
         public static final int history=0x7f050001;
         public static final int history_btn=0x7f050002;
         public static final int save_event_btn=0x7f050003;
+        public static final int txtCounter=0x7f050004;
     }
     public static final class layout {
         public static final int history=0x7f030000;
--- src/com/tracker/Main.java
+++ src/com/tracker/Main.java
@@ -3,19 +3,28 @@ package com.tracker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.view.View;
 import android.content.Intent;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.tracker.DbOpenHelper;
+
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.ContentValues;
 
-public class Main extends Activity {
+public class Main extends Activity 
+{
     /** Called when the activity is first created. */
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        SetTimer();
         
         Button history_btn = (Button) findViewById(R.id.history_btn);
         history_btn.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +44,23 @@ public class Main extends Activity {
 			    cv.put(DbOpenHelper.DATE, new Date().toString());
 			    db.insert(DbOpenHelper.TABLE_NAME, null, cv);
 			    db.close();
+			    SetTimer();
 			}
 	    });
+    }
+    
+    public void SetTimer()
+    {
+    	TextView txt = (TextView) findViewById(R.id.txtCounter);
+        DbOpenHelper dbOpenHelper = new DbOpenHelper(Main.this);
+        SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT date FROM history", null);
+        cursor.moveToLast();
+        db.close();
+
+        long lastDate = Date.parse(cursor.getString(0));        
+        Timer timer = new Timer();
+        TimerTask task = new MyTimerTask(txt, lastDate);
+        timer.schedule(task, 1, 60000);        
     }
 }
