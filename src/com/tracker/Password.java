b5f65420c379324803aff27a79c45340553d73dd
Denis <dengeruz@yahoo.com>
2011-06-07 11:43:34 +0500

    add password protection

---
+++ src/com/tracker/Password.java
@@ -0,0 +1,110 @@
+package com.tracker;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Password extends Activity implements OnClickListener
+{
+	private boolean createPassword = false;
+	private Button btn_OK;
+	private Button btn_Cancel;
+	private EditText edTxt_Password1;
+	private EditText edTxt_Password2;
+	private DbOpenHelper dbOpenHelper;
+	private SQLiteDatabase dataBase;
+	private ContentValues contentValues;
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.password);
+	        
+		createPassword = getIntent().getExtras().getBoolean("first_Load");
+		btn_OK = (Button) findViewById(R.id.btn_password_ok);
+		btn_OK.setOnClickListener(this);
+		btn_Cancel = (Button) findViewById(R.id.btn_password_cancel);
+		btn_Cancel.setOnClickListener(this);
+		edTxt_Password1  = (EditText) findViewById(R.id.edTxt_Password1);
+		edTxt_Password2  = (EditText) findViewById(R.id.edTxt_Password2);
+		if(!createPassword){
+			edTxt_Password2.setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) 
+		{
+			case R.id.btn_password_ok:
+				if(createPassword && !createPassword())
+					return;
+				else if(!checkPassword())
+					return;
+				Intent myIntent = new Intent(this, Main.class);
+	        	startActivity(myIntent);
+				break;
+			case R.id.btn_password_cancel:
+				finish();
+				Intent myIntent2 = new Intent(this, SplashScreen.class);
+	        	startActivity(myIntent2);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	private boolean checkPassword()
+	{
+		dbOpenHelper = new DbOpenHelper(this);
+    	dataBase = dbOpenHelper.getWritableDatabase();
+        Cursor cursor = dataBase.rawQuery("Select password from " 
+        		+ DbOpenHelper.TABLE_NAME_SETTINGS, null);
+        cursor.moveToLast();
+        String pass = cursor.getString(0);
+        dataBase.close();
+        if(pass.contains(edTxt_Password1.getText()))
+        	return true;
+        Toast.makeText(getApplicationContext(), "Wrong password!", 5000).show();
+        return false;
+	}
+	
+	private boolean createPassword()
+	{
+		String pass = edTxt_Password1.getText().toString();
+		if(!pass.contains(edTxt_Password2.getText()))
+		{
+			Toast.makeText(getApplicationContext(), "Wrong password!", 5000).show();
+			return false;
+		}
+		dbOpenHelper = new DbOpenHelper(getApplicationContext());
+		dataBase = dbOpenHelper.getWritableDatabase();
+		contentValues = new ContentValues();
+		contentValues.put("password", pass);
+		contentValues.put(OptionEnum.First_Load.name(), 0);
+		contentValues.put(OptionEnum.Password_Protection.name(), 1);
+		dataBase.update(DbOpenHelper.TABLE_NAME_SETTINGS, contentValues, DbOpenHelper.ID + " = " + 1, null);
+		dataBase.close();
+		return true;
+	}
+}
+
+
+/*
+dbOpenHelper = new DbOpenHelper(getApplicationContext());
+dataBase = dbOpenHelper.getWritableDatabase();
+contentValues = new ContentValues();
+contentValues.put(OptionEnum.First_Load.name(), 0);
+contentValues.put(OptionEnum.Password_Protection.name(), 1);
+contentValues.put(OptionEnum.Password_Protection.name(), 0);
+dataBase.update(DbOpenHelper.TABLE_NAME_SETTINGS, contentValues, DbOpenHelper.ID + " = " + 1, null);
+dataBase.close();
+*/
--- src/com/tracker/SplashScreen.java
+++ src/com/tracker/SplashScreen.java
@@ -3,8 +3,12 @@ package com.tracker;
 import java.util.Date;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
@@ -13,11 +17,14 @@ import android.widget.ImageView;
 
 public class SplashScreen extends Activity 
 {
-	protected boolean _active = true;
-    protected int _splashTime = 6000;
-    ImageView splash_imv;
-    AnimationDrawable mAnimation;
-    int DURATION = 100;
+	private boolean _active = true;
+	private int _splashTime = 6000;
+    private ImageView splash_imv;
+    private AnimationDrawable mAnimation;
+    private final int set_password_protection = 0;
+    private DbOpenHelper dbOpenHelper;
+    private SQLiteDatabase dataBase;
+    private ContentValues contentValues;
     
     /** Called when the activity is first created. */
     @Override
@@ -28,48 +35,50 @@ public class SplashScreen extends Activity
         /**
          * Uncomment this code if you need upgrade Database
          */
-        /*DbOpenHelper dh = new DbOpenHelper(this);
-        dh.onUpgrade(dh.getReadableDatabase(), 0, 00);
+        dbOpenHelper = new DbOpenHelper(this);
+        dbOpenHelper.onUpgrade(dbOpenHelper.getReadableDatabase(), 0, 00);
         addNewContact(new Date().toLocaleString(), "Aleksandra", new byte[0], "comments", 9, 1);
         addNewContact(new Date().toLocaleString(), "Kristina", new byte[0], "comments", 10, 2);
-        addNewContact(new Date().toLocaleString(), "Tanya", new byte[0], "comments", 9, 3);*/
+        addNewContact(new Date().toLocaleString(), "Tanya", new byte[0], "comments", 9, 3);
         
         splash_imv = (ImageView) findViewById(R.id.splash_imv);
         splash_imv.setBackgroundResource(R.animator.splash_anim);
-    	
-    	
-    	
+        
         // thread for displaying the SplashScreen
         Thread splashTread = new Thread() {
             @Override
             public void run() {
                 try {
-                	sleep(500);
+                	sleep(1000);
                     int waited = 0;
-                    //StartSplash();
+
                     mAnimation = (AnimationDrawable)splash_imv.getBackground();
                     mAnimation.setOneShot(true);
                     mAnimation.start();
-                    
                     
                     while(_active && (waited < _splashTime)) {
                         sleep(100);
                         if(_active) {
                             waited += 100;
                         }
-                    }                    
-                	finish();
-                    startActivity(new Intent(getApplicationContext(), Main.class));
+                    }
+                    mAnimation.stop();
+                    runOnUiThread(new Runnable() {
+    					@Override
+    					public void run() {
+    						splash_imv.setBackgroundResource(R.drawable.sextracker22);
+    						checkFirstLoad();
+    					}});
+                    
                     stop();
                 }
                 catch (Exception e) {
 					// TODO: handle exception
                 	finish();
-                    startActivity(new Intent(getApplicationContext(), Main.class));
 				}
             }
         };
-        splashTread.start();  
+        splashTread.start();
     }
     
     @Override
@@ -80,17 +89,78 @@ public class SplashScreen extends Activity
         return true;
     }
     
+    private void checkFirstLoad() {
+    	dbOpenHelper = new DbOpenHelper(this);
+    	dataBase = dbOpenHelper.getWritableDatabase();
+        Cursor cursor = dataBase.rawQuery("Select " + OptionEnum.First_Load.name() 
+        		+ ", " + OptionEnum.Password_Protection.name()
+        		+ " FROM " + DbOpenHelper.TABLE_NAME_SETTINGS, null);
+		cursor.moveToLast();
+		int first_Load = cursor.getInt(0);
+		int password_Protection = cursor.getInt(1);
+		dataBase.close();
+		if(first_Load == 1){
+			showDialog(set_password_protection);
+		}
+		else if(password_Protection == 1){
+			finish();
+			Intent myIntent = new Intent(getApplicationContext(), Password.class);
+        	myIntent.putExtra("first_Load", false);
+        	startActivityForResult(myIntent, 0);
+		}
+		else {
+			finish();
+            Intent myIntent = new Intent(getApplicationContext(), Main.class);
+        	startActivity(myIntent);
+		}
+	}
+     
+    @Override
+    protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case set_password_protection:
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Enable password protection?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                	Intent myIntent = new Intent(getApplicationContext(), Password.class);
+                	myIntent.putExtra("first_Load", true);
+                	startActivityForResult(myIntent, 0);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                    dbOpenHelper = new DbOpenHelper(getApplicationContext());
+                    dataBase = dbOpenHelper.getWritableDatabase();
+                    contentValues = new ContentValues();
+                    contentValues.put(OptionEnum.First_Load.name(), 0);
+                    contentValues.put(OptionEnum.Password_Protection.name(), 0);
+                    dataBase.update(DbOpenHelper.TABLE_NAME_SETTINGS, contentValues, DbOpenHelper.ID + " = " + 1, null);
+                    dataBase.close();
+                    finish();
+                    Intent myIntent = new Intent(getApplicationContext(), Main.class);
+                	startActivity(myIntent);
+                }
+            });
+            builder.setCancelable(false);
+            return builder.create();
+        default:
+        	return null;
+        }
+	}
+    
     private void addNewContact(String date, String name, byte[] photo, String comments, int rating, int position)
     {
-    	DbOpenHelper dh = new DbOpenHelper(this);
-        SQLiteDatabase db = dh.getWritableDatabase();
-        ContentValues cv = new ContentValues();
-        cv.put(DbOpenHelper.Date, date);
-        cv.put(DbOpenHelper.Name, name);
-        cv.put(DbOpenHelper.Photo, photo);
-        cv.put(DbOpenHelper.Comments, comments);
-        cv.put(DbOpenHelper.Rating, rating);
-        cv.put(DbOpenHelper.Position, position);
-        db.insert(DbOpenHelper.TABLE_NAME_HISTORY, null, cv);
+    	dbOpenHelper = new DbOpenHelper(this);
+    	dataBase = dbOpenHelper.getWritableDatabase();
+    	contentValues = new ContentValues();
+    	contentValues.put(DbOpenHelper.Date, date);
+        contentValues.put(DbOpenHelper.Name, name);
+        contentValues.put(DbOpenHelper.Photo, photo);
+        contentValues.put(DbOpenHelper.Comments, comments);
+        contentValues.put(DbOpenHelper.Rating, rating);
+        contentValues.put(DbOpenHelper.Position, position);
+        dataBase.insert(DbOpenHelper.TABLE_NAME_HISTORY, null, contentValues);
     }
 }
